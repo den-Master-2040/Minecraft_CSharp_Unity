9a912e7975618c78fb624fe4d1005d7d6f91bc30
den-Master-2040 <107744290+den-Master-2040@users.noreply.github.com>
2022-11-12 14:11:58 +0300

    chunc full

--- Assets/Chunk.cs
+++ Assets/Chunk.cs
@@ -26,11 +26,11 @@ public class Chunk : MonoBehaviour
     {
         for (int y = 0; y < VoxelData.ChunkHeight; y++)
         {
-            for (int x = 0; x < VoxelData.ChunkHeight; x++)
+            for (int x = 0; x < VoxelData.ChunkWidth; x++)
             {
-                for (int z = 0; z < VoxelData.ChunkHeight; z++)
+                for (int z = 0; z < VoxelData.ChunkWidth; z++)
                 {
-                    voxelMap[x, y, x] = true;
+                    voxelMap[x, y, z] = true;
                 }
             }
         }
@@ -40,9 +40,9 @@ public class Chunk : MonoBehaviour
     {
         for (int y = 0; y < VoxelData.ChunkHeight; y++)
         {
-            for (int x = 0; x < VoxelData.ChunkHeight; x++)
+            for (int x = 0; x < VoxelData.ChunkWidth; x++)
             {
-                for (int z = 0; z < VoxelData.ChunkHeight; z++)
+                for (int z = 0; z < VoxelData.ChunkWidth; z++)
                 {
                     AddVoxelDataToChunk(new Vector3(x, y, z));
                 }
@@ -52,22 +52,39 @@ public class Chunk : MonoBehaviour
 
     bool CheckVoxel(Vector3 pos)
     {
-         
+        int x = Mathf.FloorToInt(pos.x);
+        int y = Mathf.FloorToInt(pos.y);
+        int z = Mathf.FloorToInt(pos.z);
+
+        if (x < 0 || x > VoxelData.ChunkWidth - 1 || y < 0 || y > VoxelData.ChunkHeight - 1 || z < 0 || z > VoxelData.ChunkWidth - 1)
+            return false;
+
+        return voxelMap[x, y, z];
     }
 
     void AddVoxelDataToChunk(Vector3 pos)
     {
         for (int p = 0; p < 6; p++)
-            for (int i = 0; i < 6; i++)
+
+            if(!CheckVoxel(pos + VoxelData.faceChunks[p]))
             {
-                int triangleIndex = VoxelData.voxelTris[p, i];
-                vertices.Add(VoxelData.voxelVerts[triangleIndex] + pos);
+                vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[p, 0]]);
+                vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[p, 1]]);
+                vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[p, 2]]);
+                vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[p, 3]]);
+                uvs.Add(VoxelData.voxelUvs[0]);
+                uvs.Add(VoxelData.voxelUvs[1]);
+                uvs.Add(VoxelData.voxelUvs[2]);
+                uvs.Add(VoxelData.voxelUvs[3]);
                 triangles.Add(vertexIndex);
-
-                uvs.Add(VoxelData.voxelUvs[i]);
-
-                vertexIndex++;
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 3);
+                vertexIndex += 4;
             }
+                
     }
 
     void CreateMesh()
--- Assets/VoxelData.cs
+++ Assets/VoxelData.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class VoxelData : MonoBehaviour
 {
     public readonly static int ChunkWidth = 5;
-    public readonly static int ChunkHeight = 5;
+    public readonly static int ChunkHeight = 15;
 
     /*Vertex for cube*/
     public static readonly Vector3[] voxelVerts = new Vector3[8]
@@ -31,22 +31,20 @@ public class VoxelData : MonoBehaviour
     };
 
     /*massive for read*/
-    public static readonly int[,] voxelTris = new int[6, 6]
+    public static readonly int[,] voxelTris = new int[6, 4]
     {
-        { 0, 3, 1, 1, 3, 2 },   // Back Face
-        { 5, 6, 4, 4, 6, 7 },   // Front Face
-        { 3, 7, 2, 2, 7, 6 },   // Top Face - верхн€€ плоскость
-        { 1, 5, 0, 0, 5, 4 },   // Bottom Face
-        { 4, 7, 0, 0, 7, 3 },   // Left Face
-        { 1, 2, 5, 5, 2, 6 }    // Right Face
+        { 0, 3, 1, 2 },   // Back Face
+        { 5, 6, 4, 7 },   // Front Face
+        { 3, 7, 2, 6 },   // Top Face - верхн€€ плоскость
+        { 1, 5, 0, 4 },   // Bottom Face
+        { 4, 7, 0, 3 },   // Left Face
+        { 1, 2, 5, 6 }    // Right Face
     };
 
-    public static readonly Vector2[] voxelUvs = new Vector2[]{
+    public static readonly Vector2[] voxelUvs = new Vector2[4]{
         new Vector2(0.0f, 0.0f),
         new Vector2(0.0f, 1.0f),
         new Vector2(1.0f, 0.0f),
-        new Vector2(1.0f, 0.0f),
-        new Vector2(0.0f, 1.0f),
         new Vector2(1.0f, 1.0f),
     };
 
